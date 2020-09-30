a0f313a4fd9d8cbf6c492526b3ec84cfc1c10975
gaoyu <gaoyujack@foxmail.com>
2020-09-30 17:28:40 +0800

    feat:提交Hystrix,降级，熔断，可视化dashboard

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/smith/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,57 @@
+package com.smith.springcloud.controller;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.smith.springcloud.service.OrderHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+//@DefaultProperties(defaultFallback = "paymentGlobalFallbackMethod")
+public class OrderHystrixController {
+    @Resource
+    private OrderHystrixService orderHystrixService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = orderHystrixService.paymentInfo_OK(id);
+        log.info("*******result:" + result);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+//    @HystrixCommand
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+//    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+//        int age = 10/0;
+        String result = orderHystrixService.paymentInfo_TimeOut(id);
+        log.info("*******result:" + result);
+        return result;
+    }
+
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //全局兜底方法
+    public String paymentGlobalFallbackMethod() {
+        return "Global 我是消费者80";
+    }
+
+
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/smith/springcloud/service/OrderFallbackService.java
@@ -0,0 +1,16 @@
+package com.smith.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderFallbackService implements OrderHystrixService{
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK , (┬＿┬)";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut , (┬＿┬)";
+    }
+}
