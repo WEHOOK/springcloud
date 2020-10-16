47617b3e69c0d3acd17d06a68353d6eeb89f20bf
gaoyu <gaoyujack@foxmail.com>
2020-10-16 11:41:36 +0800

    feat:增加gateway相关

---
+++ cloud-gateway-gateway9527/src/test/java/com/smith/cloud/GateWayTest.java
@@ -0,0 +1,12 @@
+package com.smith.cloud;
+
+import org.junit.Test;
+
+import java.time.ZonedDateTime;
+
+public class GateWayTest {
+    @Test
+    public void testTime(){
+        System.out.println(ZonedDateTime.now());
+    }
+}
